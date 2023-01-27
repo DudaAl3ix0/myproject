2fcba77a6abe73f328cd063c8d6025c426aa9762
DudaAl3ix0 <116288612+DudaAl3ix0@users.noreply.github.com>
2023-01-27 18:08:40 -0300

    Banner pronto
    Foi adicionado o banner do site com modificações feitas no CSS, e utilizando o import (importe dos dados)

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,14 @@
+import './Banner.css';
+
+function Banner(){ 
+    //JSX como o React trabalha com a parte visual, parece HTML mas não é. É JSX 
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner principal da página"/>
+        </header>
+    
+    )
+
+
+}
+export default Banner
